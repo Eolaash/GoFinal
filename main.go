78016c673288e9c3d6d1363a31a49a3e5e726d15
Eolaash <blackoutat@mail.ru>
2021-02-26 17:11:16 +0400

    session interface first attempt

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
+// MAIN logic is:
+// We have multiple datasources with common interface called Session (Connect, Disconnect, Subscribe, Unsubscribe?, IsConnected?(really need?), ReadData(reading channel and parse data to common datatype))
+// Main program AS BASE variant will be CYCLED console APP with waiting some KEYBINDING or INPUT(from keyboard)
+// In main (waiting to quit cycle) part callin goroutines which read DATASOURCE channels (wss, http) through common interface and WRITING it to common DATASTORAGE interface (DB, File, Memory ect) through CHANNELS(buffered?)
+// Many aspects of flowcontrol - can't predict speed.. write interfaces to DATASTORAGE? never worked with it; OVERLOAD, OVERFLOW, DROPPED CONNECTIONS and ect;
+// SYNC and DATA protection as main - buffering to prevent dataloss? but memory depends (leaks? progession?)? SYNC speed or READ WRITE? Selfcontrol analyzer?
+// To predict READ - need more research in WEBSOCKET works (datasheets of API)
 package main
 
 import (
-	"FinalTask/testpublic"
+	"FinalTask/binance"
+	"FinalTask/poloniex"
 	"bufio"
 	"fmt"
 	"net/url"
@@ -9,57 +17,69 @@ import (
 	"strings"
 	"sync"
 	"time"
-
-	"github.com/gorilla/websocket"
 )
+
+// Session is common interface for websocket multidata sources
+type Session interface {
+	Connect(inURL url.URL) // replace with Init func >> URL - Reconnections attemts limit - Subscribe strings and ect - Timeouts and ect -> so better to use INIT func (once) and RECONNECT func as part of cycled algorythm of reading
+	Disconnect()
+	IsConnected() bool
+	Subscribe() bool
+	ReadData() string
+}
 
 func main() {
 	var wg sync.WaitGroup
+	tSessions := []Session{new(poloniex.Session), new(binance.Session)}
+	tConsoleRead := bufio.NewReader(os.Stdin)
+	tFlag := false
 
-	fmt.Println(testpublic.TestPublic())
-
-	tPoloURL := url.URL{
+	tWorkURLs := []url.URL{{
 		Scheme: "wss",
 		Host:   "api2.poloniex.com",
 		Path:   "/",
-	}
+	}, {
+		Scheme: "wss",
+		Host:   "stream.binance.com:9443",
+		Path:   "/ws",
+	}}
 
-	//tPoloURL := url.URL{
-	//	Scheme: "wss",
-	//	Host:   "stream.binance.com:9443",
-	//	Path:   "/ws",
-	//}
+	fmt.Println("Starting. Print <kill> to stop app...")
 
-	c, _, err := websocket.DefaultDialer.Dial(tPoloURL.String(), nil)
-	if err != nil {
-		fmt.Println("Oops! >> ", err.Error())
-		return
-	}
-
-	fmt.Println(sendMessage(c, []byte(`{"command": "subscribe", "channel": 1002}`)))
-	//fmt.Println(sendMessage(c, []byte(`{"method": "SUBSCRIBE", "params": ["btcusdt@aggTrade"], "id": 1}`)))
-	fmt.Println("Read #INIT-SUB > ", readMessage(c))
-
-	tConsoleRead := bufio.NewReader(os.Stdin)
-
-	tFlag := false
-
+	// CONSOLE SCAN
 	for {
 		defer wg.Done()
+		for tIndex := range tSessions {
+			defer tSessions[tIndex].Disconnect()
+		}
 
-		// tFlag for single start
+		// tFlag for JUST single start goroutines
 		if !tFlag {
-			fmt.Println("Listening subscribtion.. ")
-			tReads := 0
 			tFlag = true
 			wg.Add(1)
-			go func() {
-				for {
-					tReads++
-					fmt.Println("Read #", tReads, " > ", readMessage(c))
-					time.Sleep(time.Second * 2)
-				}
-			}()
+
+			// FOR EACH Session should start goroutines (data exchange channel is single? DATASTORAGE is single mean - is multiple channels? SYNC?)
+			for tIndex := range tSessions {
+				go func(inIndex int) {
+					for {
+						// Connection or reconnection
+						if !tSessions[inIndex].IsConnected() {
+							tSessions[inIndex].Connect(tWorkURLs[inIndex]) // subscribe should check CONNECTION
+							tSessions[inIndex].Subscribe()                 // test subs (no checks of ALREADY SUBSCRIBED (parse data neeed to complete IsSubscribed func))
+						}
+
+						// Reader
+						if tSessions[inIndex].IsConnected() {
+							fmt.Println(tSessions[inIndex].ReadData())
+						}
+
+						// Saver
+						// ...
+
+						time.Sleep(time.Second * 2) // for testing reason
+					}
+				}(tIndex)
+			}
 		}
 
 		//fmt.Print("-> ")
@@ -69,39 +89,8 @@ func main() {
 		tText = strings.Replace(tText, "\r\n", "", -1)
 
 		if "kill" == tText {
-			fmt.Println("Read #LAST > ", readMessage(c))
+			fmt.Println("Quiting app!")
 			break
 		}
 	}
-
-	fmt.Println(sendMessage(c, []byte(`{"command": "unsubscribe", "channel": 1002}`)))
-	//fmt.Println(sendMessage(c, []byte(`{"method": "UNSUBSCRIBE", "params": ["btcusdt@aggTrade"], "id": 132}`)))
-	fmt.Println("Read #UNSUB > ", readMessage(c))
-	fmt.Println("Read #UNSUB > ", readMessage(c))
-	fmt.Println("Read #UNSUB > ", readMessage(c))
-
-	//outMessage := []byte(`{"command": "subscribe", "channel": 1002}`)
-	//outMessage = []byte(`{"command": "unsubscribe", "channel": 1002}`)
-}
-
-func sendMessage(webConn *websocket.Conn, sendString []byte) string {
-
-	// send message
-	err := webConn.WriteMessage(websocket.TextMessage, sendString)
-	if err != nil {
-		return "Error on WRITE!"
-	}
-
-	return "OK"
-}
-
-func readMessage(webConn *websocket.Conn) string {
-
-	// receive message
-	_, message, err := webConn.ReadMessage()
-	if err != nil {
-		return "Error on READ1!"
-	}
-
-	return string(message)
 }
---
+++ poloniex/poloniex.go
@@ -0,0 +1,110 @@
+package poloniex
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+// Session - main control struct with defined interface
+type Session struct {
+	URL         url.URL
+	webConn     *websocket.Conn
+	Err         error
+	isConnected bool
+}
+
+// Connect - connect by websocket to inURL and writing Session struct
+func (inSession *Session) Connect(inURL url.URL) {
+	inSession.URL = inURL
+	inSession.webConn, _, inSession.Err = websocket.DefaultDialer.Dial(inSession.URL.String(), nil)
+
+	// On error
+	if inSession.Err != nil {
+		fmt.Println("Connection TRY failed >> ", inSession.Err.Error())
+		return
+	}
+
+	// Connection success
+	fmt.Println("Connection OK")
+	inSession.isConnected = true
+}
+
+// Disconnect - disconnect from active websocket
+func (inSession *Session) Disconnect() {
+
+	// Qiuck return (no action needed - already no active connection)
+	if inSession.isConnected != true || inSession.webConn == nil {
+		inSession.isConnected = false
+		fmt.Println("No connection active.")
+		return
+	}
+
+	// Disconnect
+	inSession.Err = inSession.webConn.Close()
+
+	// On error
+	if inSession.Err != nil {
+		fmt.Println("Disconnect failed >> ", inSession.Err.Error())
+		return
+	}
+
+	// Disconnect success
+	inSession.isConnected = false
+	fmt.Println("Connection DROPPED")
+}
+
+// IsConnected - quick check connection (need to be updated to ping-pong coat - check is it alive or dead)
+func (inSession *Session) IsConnected() bool {
+
+	// quick check
+	if inSession.webConn == nil {
+		inSession.isConnected = false
+	}
+
+	if inSession.isConnected == true {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Subscribe - Subscribe on predefined info channel
+func (inSession *Session) Subscribe() bool {
+
+	// SubScribe to channel (PREDEFINED - bad move? should be more flexible as IN_VAR)
+	sendString := []byte(`{"command": "subscribe", "channel": 1002}`)
+
+	// Quick check connection
+	if !inSession.IsConnected() {
+		return false
+	}
+
+	// send message
+	inSession.Err = inSession.webConn.WriteMessage(websocket.TextMessage, sendString)
+
+	// On error
+	if inSession.Err != nil {
+		fmt.Println("Subscription error >> ", inSession.Err.Error())
+		return false
+	}
+
+	// success return
+	fmt.Println("Subscription OK")
+	return true
+}
+
+// ReadData - for testing returns STRING
+func (inSession *Session) ReadData() string {
+
+	var tMessage []byte
+
+	// receive message
+	_, tMessage, inSession.Err = inSession.webConn.ReadMessage()
+	if inSession.Err != nil {
+		return "Error on READ! >> " + inSession.Err.Error()
+	}
+
+	return string(tMessage)
+}
